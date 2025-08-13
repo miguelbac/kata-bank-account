cea3afbb52fadebf274ac315f17e456dbbf17a93
Miguel Ballesteros <mbacebal@gmail.com>
2025-08-13 11:41:05 +0200

    feat : created Cuenta and its methods

--- src/main/java/dev/mba/models/BankAccount.java
+++
@@ -1,5 +0,0 @@
-package dev.mba.models;
-
-public class BankAccount {
-    
-}
---
+++ src/main/java/dev/mba/models/Cuenta.java
@@ -0,0 +1,62 @@
+package dev.mba.models;
+
+public class Cuenta {
+    protected float saldo;
+    protected int numeroConsignaciones = 0;
+    protected int numeroRetiros = 0;
+    protected float tasaAnual;
+    protected float comisionMensual = 0f;
+
+    public Cuenta(float saldo, float tasaAnual) {
+        this.saldo = saldo;
+        this.tasaAnual = tasaAnual;
+    }
+
+    public boolean consignar(float cantidad) {
+        if (cantidad <= 0f)
+            return false;
+        saldo += cantidad;
+        numeroConsignaciones++;
+        return true;
+    }
+
+    public boolean retirar(float cantidad) {
+        if (cantidad <= 0f || cantidad > saldo)
+            return false;
+        saldo -= cantidad;
+        numeroRetiros++;
+        return true;
+    }
+
+    protected float calcularInteresMensual() {
+        float tasaMensual = (tasaAnual / 12f) / 100f;
+        float interes = saldo * tasaMensual;
+        saldo += interes;
+        return interes;
+    }
+
+    public void extractoMensual() {
+        saldo -= comisionMensual;
+        calcularInteresMensual();
+    }
+
+    public String imprimir() {
+        return "Saldo: " + saldo +
+                " | Consignaciones: " + numeroConsignaciones +
+                " | Retiros: " + numeroRetiros +
+                " | Tasa anual (%): " + tasaAnual +
+                " | Comisión mensual: " + comisionMensual;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public float getComisionMensual() {
+        return comisionMensual;
+    }
+
+    public void setComisionMensual(float comisionMensual) {
+        this.comisionMensual = Math.max(0f, comisionMensual);
+    }
+}
